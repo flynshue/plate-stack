83caeb765a0d63cfe5def7891a0055c747d67c31
Felicia Lyn-Shue <flynshue@gmail.com>
2026-03-26 12:18:39 -0400

    clean up code

--- helpers.go
+++ helpers.go
@@ -16,7 +16,6 @@ type Plate struct {
 
 type PlateForm struct {
 	Plates       []Plate `schema:"plate"`
-	PlateSet     []float64
 	Barbell      float64 `schema:"barbell"`
 	TargetWeight float64 `schema:"target"`
 	Combinations [][]float64
@@ -25,51 +24,44 @@ type PlateForm struct {
 func newPlateForm() *PlateForm {
 	return &PlateForm{
 		Plates:       defaultPlateSet,
-		PlateSet:     []float64{},
 		Combinations: make([][]float64, 0),
 	}
 }
 
-func (p *PlateForm) UpdatePlateSet(plates []float64) {
-	p.PlateSet = plates
-}
-
-func (p *PlateForm) buildPlateSet() {
+func (p *PlateForm) buildPlateSet() []float64 {
 	set := make([]float64, 0)
 	for _, plate := range p.Plates {
 		for i := 0; i < plate.Quantity; i++ {
 			set = append(set, plate.Weight)
 		}
 	}
-	p.PlateSet = set
+	return set
 }
 
 func (p *PlateForm) findCombinations() {
-	if len(p.Plates) > 0 {
-		p.buildPlateSet()
-	}
+	plateSet := p.buildPlateSet()
 	target := (p.TargetWeight - p.Barbell) / 2.0
 	current := make([]float64, 0)
-	p.backTrack(target, 0, current)
+	p.backTrack(target, 0, current, plateSet)
 }
 
-func (p *PlateForm) backTrack(target float64, startIdx int, current []float64) {
+func (p *PlateForm) backTrack(target float64, startIdx int, current []float64, plateSet []float64) {
 	if target == 0.0 {
 		temp := make([]float64, len(current))
 		copy(temp, current)
 		p.Combinations = append(p.Combinations, temp)
 		return
 	}
-	for i := startIdx; i < len(p.PlateSet); i++ {
-		if i > startIdx && p.PlateSet[i] == p.PlateSet[i-1] {
+	for i := startIdx; i < len(plateSet); i++ {
+		if i > startIdx && plateSet[i] == plateSet[i-1] {
 			continue
 		}
-		if p.PlateSet[i] > target {
+		if plateSet[i] > target {
 			continue
 		}
-		current = append(current, p.PlateSet[i])
-		newTarget := target - p.PlateSet[i]
-		p.backTrack(newTarget, i+1, current)
+		current = append(current, plateSet[i])
+		newTarget := target - plateSet[i]
+		p.backTrack(newTarget, i+1, current, plateSet)
 		current = current[:len(current)-1]
 	}
 }
